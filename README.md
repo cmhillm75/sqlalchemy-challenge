b2995d627180832b373617b753b3481cd61c2d43
Chad Hillman <hillman.chad@gmail.com>
2024-12-09 17:38:36 -0600

    updated python jupyter and readme files

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # sqlalchemy-challenge
+
+Surfsup folder holds the climate_stater.ipynb and the app.py API app files.
+Resources folder holds the 2 xls csv files and the hawaii.sqlite.
+The path from the apps to resources is (../Resources/hawaii.sqlite").
+
+## Part 1 of the assignment uses climate_start.ipynb
+
+Requires use of jupyter notebook
+
+There are 3 portions of the jupyter sqlalchemy review.
+The first is import the dependencies, create the engine and establish the bases
+i.e. get the table names. Then create a session and show the columns.
+
+The second is to complete the precipitation analaysis centered around using
+dt.datetime to obtain start, end dates and then determine the interverals for plotting.
+Utilize a DataFrame and matplotlib to create the bar chart/graph where our date comes
+primarily from the 'measurement' table.
+
+The third is to utilize the 'stations' table and expand on using the dt.datetime and
+func.min/avg/max after showing the list of stations locations and their id then analyze
+the most active station 'USC00519281'. From there create a histogram of which needed
+to conver the date from a string to datetime in order to subtract from the most
+recent date.
+
+## Part 2 of the assignment uses the app.py to run the climate API
+
+Requires use of VScode, git terminal and web browser
+
+Using the framework for activities for module 10, day 3 in creating the API app,
+started the app with listing the dependencies, the datebase setup, set up Flask and
+add the run function at the end. Added os to create a db_path as was not able to get system
+to recognize the path without.
+
+The instructions list for a total of 6 routes. Using the standard route format, utilized the
+assignment instructions to name each route and commented out for each the desired outcome.
+
+The endpoints in order are:
+'homepage' ('/') lists instructions and the endpoints for all routes.
+'precipitation' ('/api/v1.0/precipitation') lists prcp data for most recent 12 months.
+'stations' ('/api/v1.0/stations') provides a json of the 9 Hawaii stations.
+'tobs' ('/api/v1.0/tobs') provides the observed times for the most active station based on the most recent year.
+'start' ('/api/v1.0/start') provides a TMIN, TMAX, TAVG from a user specified date to the end date of the dataset.
+'start_end' ('/api/v1.0/start_end') provides a TMIN, TMAX, TAVG from a user specified date range in the dataset.
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,32 +1,180 @@
 # Import the dependencies.
-
-
+from flask import Flask, jsonify
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt
+import os
 
 #################################################
 # Database Setup
-#################################################
 
-# Create engine using the `hawaii.sqlite` database file
+# Absolute path to the database file
+db_path = os.path.join(os.path.dirname(__file__), '..', 'Resources', 'hawaii.sqlite')
+engine = create_engine(f"sqlite:///{db_path}")
 
-# Declare a Base using `automap_base()`
+# Declare a Base using 'automap_base()'
+Base = automap_base()
 
 # Use the Base class to reflect the database tables
+Base.prepare(autoload_with=engine)
 
-
-# Assign the measurement class to a variable called `Measurement` and
-# the station class to a variable called `Station`
-
+# Assign the measurement class to a variable called 'Measurement' and
+# the station class to a variable called 'Station'
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create a session
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
-#################################################
-
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
-#################################################
+
+# Define the homepage route
+@app.route('/')
+def homepage():
+    return (
+        f"This is the homepage for my Climate API, please copy the links below and add dates where required:<br/><br/>"
+        f"There are 5 additional links below to provide additional information:<br/><br/>"
+        f"This is the precipitation route which provides the most recent 12 months of data in the table:<br/>"
+        f"/api/v1.0/precipitation<br/><br/>"
+        f"This is the stations route, it lists the names of the 9 stations in Hawaii:<br/>"
+        f"/api/v1.0/stations<br/><br/>"
+        f"This is the temperature observations route, it provides the dates for the most recent 12 months in the table and its temperature:<br/>"
+        f"/api/v1.0/tobs<br/><br/>"
+        f"This is the start date route, it gives temp data after the date you use. (Input a date on the end of the url in 'YYYY-MM-DD' format):<br/>"
+        f"/api/v1.0/&lt;start&gt;<br/><br/>"
+        f"This is the date range route, it gives temp data for your range of dates. (Input a start and end date in 'YYYY-MM-DD'/'YY-MM-DD' format):<br/>"
+        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;<br/>"
+    )
+###############################################
+# Precipitation route for last 12 monhts of data. 
+# Create a date/prcp dictionary and return JSON of the station data.
+@app.route('/api/v1.0/precipitation')
+def precipitation():
+    # Calculate the date 12 months ago from the most recent date in the dataset
+    recent_date_str = session.query(func.max(Measurement.date)).scalar()
+    recent_date = dt.datetime.strptime(recent_date_str, '%Y-%m-%d')
+    date_12_months_ago = recent_date - dt.timedelta(days=365)
+
+    # Query for precipitation data for the last 12 months
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= date_12_months_ago).all()
+
+    # Convert the query results to a dictionary
+    precip_data = {date: prcp for date, prcp in results}
+
+    # Return the JSON representation of the dictionary
+    return jsonify(precip_data)
+
+###############################################
+# Create stations route and return a json list of the 9 stations.
+@app.route('/api/v1.0/stations')
+def stations():
+    # Query for stations
+    results = session.query(Station.station, Station.name).all()
+    stations_data = []
+    for station, name in results:
+        stations_data.append({"station": station, "name": name})
+    return jsonify(stations_data)
+
+###############################################
+# tobs (temperature observations) route for most active station for most recent year.
+# Return a JSON list of temp observations for previous year.
+@app.route('/api/v1.0/tobs')
+def tobs():
+    # Identify the most active station
+    most_active_station = session.query(Measurement.station).group_by(Measurement.station).order_by(
+        func.count(Measurement.station).desc()).first()[0]
+
+    # Calculate the date 12 months ago from the most recent date in the dataset
+    recent_date_str = session.query(func.max(Measurement.date)).scalar()
+    recent_date = dt.datetime.strptime(recent_date_str, '%Y-%m-%d')
+    date_12_months_ago = recent_date - dt.timedelta(days=365)
+
+    # Query for temperature observations of the most active station for the last 12 months
+    results = session.query(Measurement.date, Measurement.tobs).filter(
+        Measurement.station == most_active_station).filter(Measurement.date >= date_12_months_ago).all()
+
+    # Convert the query results to a list of dictionaries
+    tobs_data = [{"date": date, "tobs": tobs} for date, tobs in results]
+
+    # Return the JSON representation of the list
+    return jsonify(tobs_data)
+
+###############################################
+# Define start route, Use one date from data and get the min, max and avg temps from that
+# date to the most recent date. See instructions on the homepage route.
+@app.route('/api/v1.0/<start>')
+def start(start):
+    try:
+        # Parse the start date
+        start_date = dt.datetime.strptime(start, '%Y-%m-%d')
+    except ValueError:
+        return jsonify({"error": "Please add a date at the end of the url in the Date format of YYYY-MM-DD"}), 400
+    
+    # Query to calculate TMIN, TAVG, and TMAX for dates greater than or equal to the start date
+    results = session.query(
+        func.min(Measurement.tobs).label('TMIN'),
+        func.avg(Measurement.tobs).label('TAVG'),
+        func.max(Measurement.tobs).label('TMAX')
+    ).filter(Measurement.date >= start).all()
+    
+    if not results or results[0].TMIN is None:
+        return jsonify({"Start Date": start, "TMIN": None, "TAVG": None, "TMAX": None}), 404
+   
+    # Create a dictionary to store the results
+    temp_stats = {
+        "Start Date": start,
+        "TMIN": results[0].TMIN,
+        "TAVG": round(results[0].TAVG, 2),
+        "TMAX": results[0].TMAX
+    }
+
+    # Return the JSON representation of the dictionary
+    return jsonify(temp_stats)
+
+###############################################
+# Define start and end route to show the min, max and avg temps for that date range.
+# This is just like the start route except we add an end date to the range.
+@app.route('/api/v1.0/<start>/<end>')
+def start_end(start, end):
+    try:
+        # Parse the start and end dates
+        start_date = dt.datetime.strptime(start, '%Y-%m-%d')
+        end_date = dt.datetime.strptime(end, '%Y-%m-%d')
+    except ValueError:
+        return jsonify({"error": "Please add start/end dates to the end of the URL in the format of YYYY-MM-DD/YYYY-MM-DD"}), 400
+
+    # Query for temperature data between the start and end dates
+    results = session.query(
+        func.min(Measurement.tobs).label('TMIN'),
+        func.avg(Measurement.tobs).label('TAVG'),
+        func.max(Measurement.tobs).label('TMAX')
+    ).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+
+    # Check if results contain None values
+    if not results or results[0].TMIN is None:
+        return jsonify({"Start Date": start, "End Date": end, "TMIN": None, "TAVG": None, "TMAX": None}), 404
+    
+    # Create a dictionary to store the results
+    temp_data = {
+        "Start Date": start,
+        "End Date": end,
+        "TMIN": results[0].TMIN,
+        "TAVG": round(results[0].TAVG, 2),
+        "TMAX": results[0].TMAX
+    }
+
+    # Return the JSON representation of the dictionary
+    return jsonify(temp_data)
+
+###############################################
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
